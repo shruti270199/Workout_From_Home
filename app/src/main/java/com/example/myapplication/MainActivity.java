d6ef819781958cc4056da668e0e02e30a61af983
Tushit Garg <tushit.23@gmail.com>
2020-04-28 02:35:04 +0530

    Added a delay of 3 seconds to the staring screen. Now second activity starts automatically Added code to prevent going to the start screen again by pressing the back button.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,28 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
-
     RelativeLayout rl1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        rl1 = (RelativeLayout)findViewById(R.id.RelativeLayout1);
-
-        rl1.setOnClickListener(new View.OnClickListener() {
+        rl1 = findViewById(R.id.RelativeLayout1);
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Intent i1 = new Intent(MainActivity.this, Main2Activity.class);
-                startActivity(i1);
+            public void run() {
+                Intent i = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(i);
+                finish();
             }
-        });
-
+        }, 3000);
     }
 }
