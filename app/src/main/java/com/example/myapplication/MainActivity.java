bf9412a273597f2ef69efb16af8e8b16d5e98a8c
Shruti <shreya27jan@gmail.com>
2020-04-27 23:42:01 +0530

    Font Family added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,28 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button b1;
+    RelativeLayout rl1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b1=(Button)findViewById(R.id.button1);
+        rl1 = (RelativeLayout)findViewById(R.id.RelativeLayout1);
 
-        b1.setOnClickListener(new View.OnClickListener() {
+        rl1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent i1 = new Intent(MainActivity.this, Main2Activity.class);
                 startActivity(i1);
-
             }
         });
-
 
     }
 }
