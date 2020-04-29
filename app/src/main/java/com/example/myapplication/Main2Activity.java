e76b079a4b955978553dd3f49773dcd9e5099e37
Tushit Garg <tushit.23@gmail.com>
2020-04-29 20:35:11 +0530

    1. Added radio group on screen 2 2. Made 2 activities one for male and one for female exercises. 3. Added intent to move to next activity according to the chosen radio button.

--- app/src/main/java/com/example/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -2,13 +2,38 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 public class Main2Activity extends AppCompatActivity {
-
+    RadioGroup radiogrp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        radiogrp = findViewById(R.id.radiogrp1);
+        Button btn = findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectid = radiogrp.getCheckedRadioButtonId();
+                if(selectid==R.id.Radiob1)
+                {
+                    Intent i = new Intent(Main2Activity.this, Main3Activity.class);
+                    startActivity(i);
+                    finish();
+                }
+                else
+                {
+                    Intent i = new Intent(Main2Activity.this, Main4Activity.class);
+                    startActivity(i);
+                    finish();
+                }
+
+            }
+        });
     }
 }
