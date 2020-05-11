8c7cba89099c7d44917c9403e27fd8014f88d5b6
Tushit Garg <tushit.23@gmail.com>
2020-05-11 17:57:18 +0530

    Added different intent passing

--- app/src/main/java/com/example/myapplication/MaleWorkoutSelection.java
+++ app/src/main/java/com/example/myapplication/MaleWorkoutSelection.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -11,12 +12,19 @@ import android.widget.Toast;
 
 public class MaleWorkoutSelection extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    int gender;
+    String exercise_type;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_male_workout_selection);
 
         Spinner spinner = findViewById(R.id.spinner_selection_male);
+
+        Intent intent = getIntent();
+        gender = intent.getIntExtra("gender",1);
+        exercise_type = intent.getStringExtra("exercise type");
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.male_selection_spinner, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -29,7 +37,8 @@ public class MaleWorkoutSelection extends AppCompatActivity implements AdapterVi
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String text = parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT);
+
+        Toast.makeText(parent.getContext(), text+exercise_type+gender, Toast.LENGTH_SHORT).show();
     }
 
     @Override
