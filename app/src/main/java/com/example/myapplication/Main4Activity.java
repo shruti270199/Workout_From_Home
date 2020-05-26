fec488807f90e56a0e340d3871b989a68ce8492a
Shruti <shreya27jan@gmail.com>
2020-05-26 17:52:23 +0530

    Gridview Added

--- app/src/main/java/com/example/myapplication/Main4Activity.java
+++ app/src/main/java/com/example/myapplication/Main4Activity.java
@@ -1,43 +1,61 @@
 package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class Main4Activity extends AppCompatActivity {
+    GridView gridview;
+
+    TextView nameText;
+    TextView descriptionText;
+    ImageView gridImages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
 
+        gridview = findViewById(R.id.grid_view);
 
-        MyListData[] myListData = new MyListData[] {
-                new MyListData("Text1","This is description 1", android.R.drawable.ic_input_add),
-                new MyListData("Text2","This is description 2", android.R.drawable.ic_btn_speak_now),
-                new MyListData("Text3","This is description 3", android.R.drawable.ic_input_delete),
-                new MyListData("Text1","This is description 1", android.R.drawable.ic_input_add),
-                new MyListData("Text2","This is description 2", android.R.drawable.ic_btn_speak_now),
-                new MyListData("Text3","This is description 3", android.R.drawable.ic_input_delete),
-                new MyListData("Text1","This is description 1", android.R.drawable.ic_input_add),
-                new MyListData("Text2","This is description 2", android.R.drawable.ic_btn_speak_now),
-                new MyListData("Text3","This is description 3", android.R.drawable.ic_input_delete),
-                new MyListData("Text1","This is description 1", android.R.drawable.ic_input_add),
-                new MyListData("Text2","This is description 2", android.R.drawable.ic_btn_speak_now),
-                new MyListData("Text3","This is description 3", android.R.drawable.ic_input_delete),
-        };
+        CustomAdapter customAdapter = new CustomAdapter();
+        gridview.setAdapter(customAdapter);
+    }
 
+    private class CustomAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return 0;
+        }
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-        MyListAdapter adapter = new MyListAdapter(myListData);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = getLayoutInflater().inflate(R.layout.grid_row_data, null);
+
+            nameText = view.findViewById(R.id.grid_text1);
+            descriptionText = view.findViewById(R.id.grid_text2);
+            gridImages = view.findViewById(R.id.grid_img);
+
+            //setText and setImageResource
+            return view;
+
+        }
     }
 }
